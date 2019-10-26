2f9347884a71a3ab6d28a6d8684cd00cea8b68e1
gottfriedoetzi <henry.apostle@gmx.de>
2019-10-26 15:57:47 +0200

    Blatt 2.2 Aufgabe 1 Geschickter wäre es noch AvgValImg aus Blatt2.1 zu importieren. So konnte ich es aber noch mal neu schreiben, dass es alle Pixel durchläuft und hochzählt

--- Termin 2/Aufgabenblatt 2.2/Aufgabe 1.py
+++ Termin 2/Aufgabenblatt 2.2/Aufgabe 1.py
@@ -9,16 +9,25 @@ def BildVergleich (img1, img2):
     :param img2:
     :return:
     """
-    unterschied = np.zeros(img1.shape, dtype=np.uint8) #TODO: was tun bei unterschiedlichen Bildgrößen?
-    for x in range(img1.shape[0]):
-        for y in range(img1.shape[1]):
-            unterschied[x,y] = np.sqrt(img1[x,y]**2 + img2[x,y]**2)
-    return unterschied
+    return np.abs(AvgValImg(img1)-AvgValImg(img2))
 
-img1 = imread("./satBilder/agri3.png")
-img2 = imread("./satBilder/agri6.png")
-unterschied = BildVergleich(img1, img2)
+def AvgValImg(img):
+    """
 
-plt.imshow(unterschied, cmap="Greys_r")
+    :param img:
+    :return:
+    """
+    n= 0
+    for x in range(img.shape[0]):
+        for y in range(img.shape[1]):
+            n += img[x,y]
+    n = n/(img.shape[0]*img.shape[1])
+    return n
 
-#Aufgabe voll falsch verstanden, ich mach das noch richtig :D
+agri3 = imread("./satBilder/agri3.png")
+agri6 = imread("./satBilder/agri6.png")
+urban1 = imread("./satBilder/urban1.png")
+
+print("Unterschied zwischen agri3 und agri6:", BildVergleich(agri3,agri6))
+print("Unterschied zwischen agri3 und urban1:", BildVergleich(agri3,urban1))
+print("Unterschied zwischen agri6 und urban1:", BildVergleich(agri6,urban1))
